ad8843a24fc01bdd7af7e6b06f3c11795d335ee5
connortbot <loiconnor8@gmail.com>
2023-11-24 04:05:20 -0500

    Incorporate socket into server

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 const cors = require('cors');
+
 const gameRoutes = require('./routes/gameRoutes.js');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
 const port = 5000;
 
 app.use(express.json());
@@ -14,6 +24,20 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('joinGame', (gameId) => {
+    socket.join(gameId);
+    console.log(`A player joined game: ${gameId}`);
+  });
+
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
